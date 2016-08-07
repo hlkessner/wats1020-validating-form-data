1cda379fe0f4568092408c95c56ca37885b0a2c8
kessnerh <kessnerh@seattlu.edu>
2016-08-07 18:20:24 -0400

    Finished form validation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
 /* Custom JS goes here. */
 
 // For this assignment you'll need to do a few things:
+
 // 1. Create a document ready handler.
+// Create a document ready handler.
+$(document).on('ready', function(){
+ $('#order-form').validate({
+   submitHandler:function(form){
+     form.submit();
+   },
+   rules: {
+     "your-name": {
+       required: true,
+       maxlength: 128
+     },
+
+     "your-zip": {
+       required: true,
+       digits: true,
+       maxlength: 5
+     },
+
+     "your-state": {
+       required: true,
+       maxlength: 2
+     },
+
+     "card-holder-name": {
+       required: true,
+       maxlength: 128
+     },
+
+     "card-number": {
+       required: true,
+       creditcard: true
+     },
+
+     "cvv": {
+       required: true,
+       maxlength: 3,
+       digits: true
+     }
+   }
+ });
+$('label span-glyphicon').tooltip();
+});
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
 
